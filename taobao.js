1e7afdeba66a74ec4ecc8da35fd8554a6699e8e4
dadadadashan <m15953137660@163.com>
2019-11-06 16:48:05 +0800

    Update taobao.js

--- taobao.js
+++ taobao.js
@@ -1,69 +1,149 @@
 toast("检测是否开启无障碍模式")
 auto.waitFor()
 var appName = "手机淘宝";
-var shops = ["欧莱雅官方旗舰店", "GREE格力官方旗舰店", "苏泊尔官方旗舰店", "小米官方旗舰店",
-    "荣耀官方旗舰店", "vivo官方旗舰店", "OPPO官方旗舰店", "李宁官方网店", "olay官方旗舰店", "YSL圣罗兰美妆官方旗舰店",
-    "蒙牛旗舰店", "自然堂旗舰店", "KIEHL'S科颜氏官方旗舰店", "Lancome兰蔻官方旗舰店", "雅诗兰黛官方旗舰店天猫店",
-    "美特斯邦威官方网店", "宝洁官方旗舰店", "adidas官方旗舰店", "奥克斯旗舰店", "海尔官方旗舰店", "HR赫莲娜官方旗舰店",
-    "阿玛尼美妆官方旗舰店", "SK-II官方旗舰店", "百雀羚旗舰店", "戴森官方旗舰店", "ZARA官方旗舰店", "波司登官方旗舰店"]
-sleep(2000);
+var shops = ["#3001646806"]
+sleep(1000);
 launchApp(appName);
-sleep(4000);
-//寻找领喵币按钮，存在则执行任务，否则退出脚本
-var lingmiaobi = indexInParent(5).depth(18).text("").findOnce();
-if (lingmiaobi) {
-    lingmiaobi.click();
-    sleep(1000);
+sleep(5000);
+
+//start
+
+if(clickLMB()) {
+    execTask();
+}
+else {
+    toast("未检查到领喵币按钮,自动跳转页面")
+    clickEntrance();
+    indexInParent(5).depth(18).text("领喵币").waitFor();
+    if(clickLMB()) {
+        execTask();
+    }
+    else{
+       toast("网速较慢，失败")
+    }
+}
+sleep(500);
+execShopCheckin(shops);
+toast("检查其余任务是否完成");
+sleep(500);
+clickEntrance();
+indexInParent(5).depth(18).text("领喵币").waitFor();
+if(clickLMB()) {
     execTask();
 }
 else {
     toast("未检查到领喵币按钮")
-}
-toast("即将执行店铺签到任务");
-sleep(1000);
-execShopCheckin(shops);
-toast("签到任务完成，即将检查其余任务是否完成");
-var aa=className("android.widget.FrameLayout").depth(12).drawingOrder(8).findOne();
-click((aa.bounds().left + aa.bounds().right) / 2, (aa.bounds().top + aa.bounds().bottom) / 2);
-sleep(4000);
-var ling = indexInParent(5).depth(18).text("").untilFind();
-if (ling) {
-    ling.click();
-    sleep(1000);
-    execTask();
-}
+} 
+sleep(500);
 
 toast("任务完成，感谢支持")
 
+
+//活动入口
+function clickEntrance()
+{
+    var aa=descContains("捉猫猫").findOne() || className("android.widget.LinearLayout").depth(10).drawingOrder(6).indexInParent(5).findOne();
+    click((aa.bounds().left + aa.bounds().right) / 2, (aa.bounds().top + aa.bounds().bottom) / 2);
+}
+
+//寻找领喵币按钮，存在则执行任务，否则退出脚本
+function clickLMB () {
+    //寻找领喵币按钮，存在则执行任务，否则退出脚本
+    var lingmiaobi = indexInParent(5).depth(18).text("领喵币").findOnce();
+    if (lingmiaobi) {
+        lingmiaobi.click();
+        sleep(1000);
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function closeTaskTab() {
+    var closeBtn = className("android.widget.Button").depth(18).findOnce();
+    if(closeBtn) {
+        closeBtn.click();
+        sleep(2000);
+        return true
+    }
+    else {
+        return false
+    }
+}
 function execTask() {
     while (true) {
         sleep(1000);
-        var target = text("去进店").findOne(2000) || text("去浏览").findOne(500) || text("签到").findOne(500);
+        var target = text("去浏览").findOne(2000) || text("签到").findOne(500) || text("去进店").findOne(500);
+        //var target = text("已完成").findOne();
         if (target == null) {
-            back();
-            sleep(1000);
-            break;
+            //判断是否隐藏了20个店铺浏览
+            var dianpu = textContains("/20").findOne(2000);
+            if(dianpu)
+            {
+                //toast("浏览任务完成");
+                back();
+                sleep(1000);
+                break;
+            }
+            else{
+                toast("任务被隐藏，尝试重新打开");
+                back();
+                sleep(500);
+                clickEntrance();
+                indexInParent(5).depth(18).text("领喵币").waitFor();
+                clickLMB();
+                continue;
+            }
         }
         target.click();
-        sleep(3000);
+        sleep(1000);
         if (target.text() === "签到") {
             sleep(2000);
             continue;
         }
         else {
-            //执行浏览广告类任务
-            viewWeb(20);
+            //执行浏览广告类任务,返回值为false表示执行任务异常
+            var flag = viewWeb(20);
+            if(!flag) {
+                clickEntrance();
+                indexInParent(5).depth(18).text("领喵币").waitFor();
+                clickLMB();
+            }
         }
-        back();
     }
 }
 function viewWeb(time) {
+    sleep(300);
     gesture(1000, [300, device.height - 300], [300, device.height - 500]);
     var cnt = 1;
     while (true) {
-        var finish = desc("任务完成").exists() || descStartsWith("已获得").exists()
-        if (finish || cnt > time) {
-            break;
+        var finish = desc("任务完成").exists()||text("任务完成").exists() || descContains("已获得").exists() || textContains("今日已达上限").exists() || descContains("今日已达上限").exists();
+        if(finish && cnt <= 10) {
+            //表示出现异常，需要重新打开任务栏
+            back();
+            toast("请等待程序自检完成")
+            sleep(500);
+            back();
+            //回到首页
+            sleep(500);
+            return false
+        }
+        if (finish || cnt > time) { 
+            var enterGameBtn = desc("捉猫猫").findOnce();
+            if(enterGameBtn) {
+                //toast("当前位置异常，尝试修复")
+                execShopCheckin(shops);
+                sleep(1000);
+                enterGameBtn.click();
+                indexInParent(5).depth(18).text("领喵币").waitFor();
+                clickLMB();
+            }
+            else{
+                back();
+                sleep(500);
+            }
+            return true
         }
         sleep(1000);
         cnt += 1;
@@ -81,17 +161,22 @@ function execShopCheckin(shopName) {
         toast("未找到搜索按钮")
     }
 }
+
 function shopCheckin(shopName) {
     for (var i = shopName.length - 1; i >= 0 ; --i) {
         //将搜索店铺名并点击搜索
-        sleep(2000);
-        var bar = id("search_bar_wrapper").findOne(1000);
-        if(bar)
-            bar.click();
-        sleep(1000);
-        searchBar2 = className("android.widget.EditText").depth(10).indexInParent(0).findOnce();
-        searchBar2.setText(shopName[i]);
-        sleep(1000);
+        //sleep(2000);
+        //sleep(2000);
+        while(1)
+        {
+            sleep(100);
+            searchBar2 = className("android.widget.EditText").depth(10).indexInParent(0).findOnce();
+            if(searchBar2)
+            {
+                 searchBar2.setText(shopName[i]);
+                 break;
+            }
+        }
         var sear = id("searchbtn").findOne(5000);
         if(sear)
             sear.click();
@@ -99,54 +184,21 @@ function shopCheckin(shopName) {
             break;
         }
         sleep(1000);
-        //点击店铺
-        var shopText = text("店铺").findOne(5000);
-        //var currentShop = shopText.parent().parent().parent();
-        //currentShop.click();
-        click((shopText.bounds().left + shopText.bounds().right) / 2, (shopText.bounds().top + shopText.bounds().bottom) / 2);
-        sleep(1000);
-        //className("android.support.v7.app.ActionBar$b").clickable(true).selected(true).findOne(3000).click();
-        //点击进店
-        // var jindian = depth(15).indexInParent(0).text("进店").findOne(3000).parent();
-         var jindian = className("android.widget.FrameLayout").descContains(shopName[i]).clickable(true).findOne(2000) || 
-            textContains(shopName[i]).clickable(true).findOne(2000) ||
-            depth(18).indexInParent(1).descContains(shopName[i]).findOne(3000) 
-        if (jindian) {  
-            click((jindian.bounds().left + jindian.bounds().right) / 2, (jindian.bounds().top + jindian.bounds().bottom) / 2);
-            //双十一父亲 indexinparent(1) depth(10)
-            sleep(500);
-            //点击双十一
-            //var childofBtn = classNameStartsWith("android.widget.ImageView").depth(17).indexInParent(0).findOne(10000);
-            var childofBtn = classNameStartsWith("android.widget.FrameLayout").depth(15).drawingOrder(3).findOne(5000);
-            if (childofBtn) {
-                var btn = childofBtn;
-                // var btn = classNameStartsWith("android.widget.FrameLayout").depth(15).indexInParent(2).clickable(true).findOne(5000);
-
-                btn.click();
-                //点击签到领喵币
-                sleep(1000);
-                var qiandaoImg = desc("签到领喵币").findOne(5000) || text("签到领喵币").findOne(1000);
-                if (qiandaoImg) {
-                    //var qiandao = qiandaoImg.parent();
-                    //qiandao.click();
-                    click((qiandaoImg.bounds().left + qiandaoImg.bounds().right) / 2, (qiandaoImg.bounds().top + qiandaoImg.bounds().bottom) / 2);
-                    sleep(1000);
-                    log(shopName[i] + "签到领喵币");
-                }
-                else {
-                    log(shopName[i] + "没找到领喵币按钮")
-                }
-            }
-            else {
-                log(shopName[i] + "没找到双十一按钮")
+        if (i === shopName.length - 1) {
+            var helpme = text("为TA助力").findOne(5000);
+            if (helpme) {
+                click((helpme.bounds().left + helpme.bounds().right) / 2, (helpme.bounds().top + helpme.bounds().bottom) / 2);
+                sleep(500);
             }
             back();
-        }
-        else {
-            log(shopName[i] + "没找到进店按钮");
+            sleep(300);
             back();
+            sleep(500)
+            back();
+            continue;
         }
     }
     back();
-    back();
+
 }
+
